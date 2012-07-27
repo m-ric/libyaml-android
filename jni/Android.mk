27c75c31b997733ae5c152e1cdae92030ee37ebf
Emeric Vigier <emeric.vigier@savoirfairelinux.com>
2012-07-27 10:39:01 -0400

    initial commit

---
+++ jni/Android.mk
@@ -0,0 +1,16 @@
+# libyaml/jni/Android.mk
+
+LOCAL_PATH := $(call my-dir)
+include $(CLEAR_VARS)
+ 
+LOCAL_CFLAGS := -DYAML_VERSION_STRING=\"0.1.4\" \
+				-DYAML_VERSION_MAJOR=0 \
+				-DYAML_VERSION_MINOR=1 \
+				-DYAML_VERSION_PATCH=4
+LOCAL_MODULE     := libyaml                 # name of your module
+LOCAL_LDLIBS     := -L$(SYSROOT)/usr/lib    # libraries to link against
+LOCAL_SRC_FILES  := yaml_private.h api.c reader.c scanner.c \
+					parser.c loader.c writer.c emitter.c dumper.c
+
+include $(BUILD_SHARED_LIBRARY)            # uncomment this line to build a shared library
+#include $(BUILD_STATIC_LIBRARY)           # here, we are building a static library
